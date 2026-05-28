21deaf9c092dbab6c4a4327ce6c6f8ce755986f7
minuk12.choi <minuk12.choi@gmail.com>
2026-05-28 14:10:41 +0900

    Delete 2_Compatibility/facts/config directory

--- 2_Compatibility/facts/config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from django.views.generic import RedirectView
-
-urlpatterns = [
-    path("", RedirectView.as_view(url="/facts/dashboard/", permanent=False)),
-    path("admin/", admin.site.urls),
-    path("facts/", include(("facts.urls", "facts"), namespace="facts")),
-]
