d8d40a8f1299c88327caaa4fa5ea01dd285c97a5
minuk12.choi <minuk12.choi@gmail.com>
2026-05-28 14:03:47 +0900

    Create legacy.py

---
+++ 2_Compatibility/facts/view_modules/legacy.py
@@ -0,0 +1,7 @@
+"""View legacy facade: 하위 기능 모듈 재노출 전용."""
+from .dashboard import *
+from .history import *
+from .prevent_tip import *
+from .master import *
+from .kpi import *
+from .voc import *
