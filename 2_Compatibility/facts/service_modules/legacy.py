84ceee6c7f752862976111d4bcf3bcb26b16b60c
minuk12.choi <minuk12.choi@gmail.com>
2026-05-28 14:07:17 +0900

    Create legacy.py

---
+++ 2_Compatibility/facts/service_modules/legacy.py
@@ -0,0 +1,8 @@
+"""Service legacy facade: 기능 모듈 재노출 전용."""
+from .dashboard_filters import *
+from .dashboard_rows import *
+from .plan_detail import *
+from .tip_missing import *
+from .kpi import *
+from .master import *
+from .bulk_upload import *
